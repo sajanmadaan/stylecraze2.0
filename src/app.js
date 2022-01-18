dcc67badf50b802c400ac2ddf82f39ea2c3777b2
Durgesh kumar rai <durgeshrai633@gmail.com>
2022-01-18 17:10:10 +0530

    index file is coming correctly
    type  localhost:5000 on browser search bar and the main page should be visible

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const indexController = require("./controllers/index.controller");
+app.set("view engine", "ejs");
+app.use("/", indexController);
 app.listen(port, () => {
   console.log("Listening on port " + port);
 });

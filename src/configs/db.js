2a9835e05cf91b179d44020ca677d04d48b9318d
dimple06varshney <dimplevarshney06@gmail.com>
2022-01-19 12:07:30 +0530

    atlas database connected

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config()
 module.exports = () => {
-  return mongoose.connect("mongodb://127.0.0.1:27017/stylecraze");
+  return mongoose.connect(process.env.DATABASE_LINK);
 };

8ab544c74833751acbe749200047baa61f48fb6f
Sajan Madaan <madanraghav7@gmail.com>
2022-03-21 13:25:19 +0530

    edit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require("./index");
-
+const env = require("dotenv");
 
 const connect = require("./src/configs/db")
 
-app.listen(2545, async () => {
+app.listen(process.env.PORT||2545, async () => {
   try {
     await connect();
     console.log("listening on port 2545");
